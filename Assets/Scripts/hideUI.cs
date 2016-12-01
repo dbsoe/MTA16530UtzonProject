b625d008bb188cae47b098cafcac127259551ee1
dbsoe <dbsa11@student.aau.dk>
2016-12-01 13:22:25 +0100

    Johans changes
    I
    m just commiting it, I have no idea whats inside

---
+++ Assets/Scripts/hideUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hideUI : MonoBehaviour
+{
+	SteamVR_TrackedObject trackedObj;
+	public GameObject Canvas;
+	public bool active = true;
+
+	void Awake ()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var device = SteamVR_Controller.Input ((int)trackedObj.index);
+		if (device.GetTouchDown (SteamVR_Controller.ButtonMask.ApplicationMenu)) {
+			Debug.Log ("Touchdown");
+			Canvas.SetActive (!active);
+			active = !active;
+		} 
+	}
+}
