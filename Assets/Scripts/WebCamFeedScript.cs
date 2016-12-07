c69639f1a496e9bcb59bbc266dcc61149388cd0b
dbsoe <dbsa11@student.aau.dk>
2016-12-07 14:47:00 +0100

    EdgeDetection implemented into the scene
    As soon as I get access to a mouse i'll even make sure to move the
    primitives to the right place.

---
+++ Assets/Scripts/WebCamFeedScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WebCamFeedScript : MonoBehaviour
+{
+
+    public Material material;
+    
+    void Start ()
+    {
+       WebCamDevice[] devices = WebCamTexture.devices;
+
+       Renderer rend = this.GetComponentInChildren<Renderer>();
+
+       // assuming the first available WebCam is desired
+       WebCamTexture tex = new WebCamTexture(devices[0].name);
+       rend.material.mainTexture = tex;
+
+       material.mainTexture = tex;
+       
+       tex.Play();
+    }
+
+}
